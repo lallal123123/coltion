f3e79170d4d96a2654458a8d8cd9ce8434b2a049
jun <wnsgud0202@naver.com>
2024-09-24 23:41:42 +0900

    teamspace crud complete

--- src/main/java/com/coltion/coltion/dao/SpaceDao.java
+++ src/main/java/com/coltion/coltion/dao/SpaceDao.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.coltion.coltion.dto.TeamspaceDto;
 import com.coltion.coltion.dto.WorkspaceDto;
 @Mapper
 public interface SpaceDao {
 
+	////워크스페이스 
 	//워크스페이스 등록
 	public void insertWorkspace(@Param("dto") WorkspaceDto workspace);
 	
@@ -23,4 +25,18 @@ public interface SpaceDao {
 	
 	//워크스페이스 삭제
 	public void deleteWorkspace(@Param("workspaceNo") String workspaceNo);
+	
+	////팀스페이스
+	//팀스페이스 등록
+	public void insertTeamspace(@Param("dto") TeamspaceDto teamspaceDto);
+	
+	//워크스페이스별 팀스페이스 조회(by workspaceNo)
+	public List<TeamspaceDto> getTeamspaceByWorkspaceNo(@Param("workspaceNo")String workspaceNo);
+	
+	//팀스페이스 이름 수정
+	public void  updateTeamspace(@Param("dto") TeamspaceDto teamspaceDto);
+	
+	//팀스페이스 삭제
+	public void deleteTeamspace(@Param("teamspaceNo") String teamspaceNo);
+	
 }
---
+++ src/main/java/com/coltion/coltion/dto/TeamspaceDto.java
@@ -0,0 +1,17 @@
+package com.coltion.coltion.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeamspaceDto {
+
+	private Long teamspaceNo;
+	private String teamspaceName;
+	private Long workspaceNo;
+}
